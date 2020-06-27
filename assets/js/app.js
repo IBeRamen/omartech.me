e0d6481f881d147172b2a3dbe3d487856ec3d374
IBeRamen <iberamen@gmail.com>
2020-06-26 20:20:27 -0400

    Removed animation on view on footer due to mobile view issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,13 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 var nodeArray = [
-    document.querySelector("#intro"),
-    document.querySelector("#about"),
-    document.querySelector("#projects"),
-    document.querySelector("#contact")
+  document.querySelector("#intro"),
+  document.querySelector("#about"),
+  document.querySelector("#projects"),
 ];
 
 ScrollReveal().reveal(nodeArray);
-ScrollReveal().reveal('.nodeList');
-ScrollReveal().reveal('.card');
+ScrollReveal().reveal(".nodeList");
+ScrollReveal().reveal(".card");
